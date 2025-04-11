90aac05bd70bdf9fcc9b74837273e5026ee82362
lw <linhtm@konistudio.xyz>
2025-04-11 18:11:53 +0700

    chore: Temporarily add sepolia_ethereum support

--- packages/extension-base/src/services/chain-service/constants.ts
+++ packages/extension-base/src/services/chain-service/constants.ts
@@ -10,6 +10,7 @@ export const API_MAX_RETRY = 2;
 
 export const _AVAIL_APP_CHAINS_WHITELIST: string[] = [
   'avail_mainnet',
+  'sepolia_ethereum',
   'availTuringTest',
   'goldberg_testnet',
   'ethereum',
